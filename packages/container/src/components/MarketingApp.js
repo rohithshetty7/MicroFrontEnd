d10561f43e5f7039499f10d7aa225cb7235838fe
Rohith <rohithshettykundapura22@gmail.com>
2026-04-25 22:21:59 +0530

    Refactor MarketingApp and bootstrap to support navigation with history prop

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,23 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const histrroy = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        console.log("The container noticed navigation in Markting!!");
+        const { pathname } = histrroy.location;
+        if (pathname !== nextPathname) {
+          histrroy.push(nextPathname);
+        }
+      }
+    });
+    histrroy.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -4,9 +4,21 @@ import App from './App';
 import { createBrowserHistory } from 'history';
 
 // Mount function to start up the app
-const mount = (el) => {
-  const history = createBrowserHistory();
-  ReactDOM.render(<App  history={history}/>, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createBrowserHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+
+    }
+  }
 };
 
 // If we are in development and in isolation,
@@ -15,7 +27,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
